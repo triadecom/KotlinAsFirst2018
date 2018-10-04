d525899e9ea4b9a9008b17401ff2781992ae1cbb
Vladislav <43061844+triadecom@users.noreply.github.com>
2018-10-04 23:51:08 +0300

    Add files via upload

---
+++ Loop.kt
@@ -0,0 +1,350 @@
+@file:Suppress("UNUSED_PARAMETER")
+package lesson3.task1
+
+import kotlinx.html.MATH
+import kotlin.math.sqrt
+import kotlin.math.pow
+import kotlin.math.*
+
+/**
+ * Пример
+ *
+ * Вычисление факториала
+ */
+fun factorial(n: Int): Double {
+    var result = 1.0
+    for (i in 1..n) {
+        result = result * i // Please do not fix in master
+    }
+    return result
+}
+
+/**
+ * Пример
+ *
+ * Проверка числа на простоту -- результат true, если число простое
+ */
+fun isPrime(n: Int): Boolean {
+    if (n < 2) return false
+    if (n == 2) return true
+    if (n % 2 == 0) return false
+    for (m in 3..sqrt(n.toDouble()).toInt() step 2) {
+        if (n % m == 0) return false
+    }
+    return true
+}
+
+/**
+ * Пример
+ *
+ * Проверка числа на совершенность -- результат true, если число совершенное
+ */
+fun isPerfect(n: Int): Boolean {
+    var sum = 1
+    for (m in 2..n/2) {
+        if (n % m > 0) continue
+        sum += m
+        if (sum > n) break
+    }
+    return sum == n
+}
+
+/**
+ * Пример
+ *
+ * Найти число вхождений цифры m в число n
+ */
+fun digitCountInNumber(n: Int, m: Int): Int =
+        when {
+            n == m -> 1
+            n < 10 -> 0
+            else -> digitCountInNumber(n / 10, m) + digitCountInNumber(n % 10, m)
+        }
+
+/**
+ * Тривиальная
+ *
+ * Найти количество цифр в заданном числе n.
+ * Например, число 1 содержит 1 цифру, 456 -- 3 цифры, 65536 -- 5 цифр.
+ *
+ * Использовать операции со строками в этой задаче запрещается.
+ */
+fun digitNumber(n: Int): Int {
+    var x = n;
+    var result = 0;
+    do {
+        result++;
+        x/=10;
+    } while (x != 0)
+
+
+return result;
+}
+
+/**
+ * Простая
+ *
+ * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
+ * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
+ */
+fun fib(n: Int): Int {
+    if (n == 1 || n == 2) return 1
+    return fib(n-1) + fib(n-2)
+}
+
+/**
+ * Простая
+ *
+ * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
+ * минимальное число k, которое делится и на m и на n без остатка
+ */
+fun lcm(m: Int, n: Int): Int {
+    var greater_num = max(m,n)
+    var d = greater_num
+    while (!(greater_num.rem(m) == 0 && greater_num.rem(n) == 0))
+        greater_num += d
+    return greater_num
+}
+
+/**
+ * Простая
+ *
+ * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
+ */
+fun minDivisor(n: Int): Int {
+    for (i in 2..n) {
+        if (n.rem(i) == 0) return i
+    }
+    return n
+}
+
+/**
+ * Простая
+ *
+ * Для заданного числа n > 1 найти максимальный делитель, меньший n
+ */
+fun maxDivisor(n: Int): Int {
+    for (i in n-1..1) {
+        if (n.rem(i) == 0) return i
+    }
+    return 1
+}
+
+/**
+ * Простая
+ *
+ * Определить, являются ли два заданных числа m и n взаимно простыми.
+ * Взаимно простые числа не имеют общих делителей, кроме 1.
+ * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
+ */
+fun isCoPrime(m: Int, n: Int): Boolean {
+    var greater_one = max(m,n)
+    var lesser_one = min(m,n)
+    for (i in 2..lesser_one+1) {
+        if (lesser_one.rem(i) == 0)
+            if (greater_one.rem(i) == 0) return false
+    }
+    return true
+}
+
+/**
+ * Простая
+ *
+ * Для заданных чисел m и n, m <= n, определить, имеется ли хотя бы один точный квадрат между m и n,
+ * то есть, существует ли такое целое k, что m <= k*k <= n.
+ * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
+ */
+fun squareBetweenExists(m: Int, n: Int): Boolean {
+    for (i in m..n+1) {
+        if (sqrt(i.toDouble()) == truncate(sqrt(i.toDouble())))
+            return true
+    }
+    return false
+}
+
+
+/**
+ * Средняя
+ *
+ * Гипотеза Коллатца. Рекуррентная последовательность чисел задана следующим образом:
+ *
+ *   ЕСЛИ (X четное)
+ *     Xслед = X /2
+ *   ИНАЧЕ
+ *     Xслед = 3 * X + 1
+ *
+ * например
+ *   15 46 23 70 35 106 53 160 80 40 20 10 5 16 8 4 2 1 4 2 1 4 2 1 ...
+ * Данная последовательность рано или поздно встречает X == 1.
+ * Написать функцию, которая находит, сколько шагов требуется для
+ * этого для какого-либо начального X > 0.
+ */
+fun collatzSteps(x: Int): Int {
+    var i = 0
+    var next = x
+    while (next != 1) {
+        if (next.rem(2) == 0)
+            next = next / 2
+        else
+            next = 3 * next + 1
+        i++
+    }
+    return i
+}
+
+/**
+ * Средняя
+ *
+ * Для заданного x рассчитать с заданной точностью eps
+ * sin(x) = x - x^3 / 3! + x^5 / 5! - x^7 / 7! + ...
+ * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю
+ */
+fun sin(x: Double, eps: Double): Double = TODO()
+
+/**
+ * Средняя
+ *
+ * Для заданного x рассчитать с заданной точностью eps
+ * cos(x) = 1 - x^2 / 2! + x^4 / 4! - x^6 / 6! + ...
+ * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю
+ */
+fun cos(x: Double, eps: Double): Double = TODO()
+
+/**
+ * Средняя
+ *
+ * Поменять порядок цифр заданного числа n на обратный: 13478 -> 87431.
+ *
+ * Использовать операции со строками в этой задаче запрещается.
+ */
+fun revert(n: Int): Int {
+    var m = n
+    var result = 0;
+    while (m != 0){
+        result = result * 10 + (m % 10);
+        m/=10
+
+    }
+    return result
+}
+
+/**
+ * Средняя
+ *
+ * Проверить, является ли заданное число n палиндромом:
+ * первая цифра равна последней, вторая -- предпоследней и так далее.
+ * 15751 -- палиндром, 3653 -- нет.
+ *
+ * Использовать операции со строками в этой задаче запрещается.
+ */
+fun isPalindrome(n: Int): Boolean = n == revert(n);
+
+
+
+/**
+ * Средняя
+ *
+ * Для заданного числа n определить, содержит ли оно различающиеся цифры.
+ * Например, 54 и 323 состоят из разных цифр, а 111 и 0 из одинаковых.
+ *
+ * Использовать операции со строками в этой задаче запрещается.
+ */
+fun hasDifferentDigits(n: Int): Boolean {
+    if (n < 10) return false
+    var nn = n
+    var prev_dig = nn.rem(10)
+    nn = nn.div(10)
+    while (nn > 0) {
+        if (nn.rem(10) != prev_dig)
+            return true
+        prev_dig = nn.rem(10)
+        nn = nn.div(10)
+    }
+    return false
+}
+
+/**
+ * Сложная
+ *
+ * Найти n-ю цифру последовательности из квадратов целых чисел:
+ * 149162536496481100121144...
+ * Например, 2-я цифра равна 4, 7-я 5, 12-я 6.
+ *
+ * Использовать операции со строками в этой задаче запрещается.
+ */
+fun squareSequenceDigit(n: Int): Int {
+    var len = 0;
+    var i = 1;
+    var m = n;
+    while (true) {
+        var res = i * i
+        if (len + len(res) >= m) {
+            var ans = revert(res);
+
+            while (len + 1 < m) {
+                ans = ans.div(10);
+                len++;
+            }
+            return ans.rem(10);
+        }
+        len = len + len(res)
+        i++
+    }
+}
+fun len(n: Int): Int {
+    var m = n;
+    var res = 0;
+    while (m > 0) {
+        m = m.div(10);
+        res++;
+    }
+    return res;
+}
+
+/**
+ * Сложная
+ *
+ * Найти n-ю цифру последовательности из чисел Фибоначчи (см. функцию fib выше):
+ * 1123581321345589144...
+ * Например, 2-я цифра равна 1, 9-я 2, 14-я 5.
+ *
+ * Использовать операции со строками в этой задаче запрещается.
+ */
+fun fibSequenceDigit(n: Int): Int {
+    var len = 0;
+    var i = 1;
+    var m = n;
+    while (true) {
+        var res = fib(i)
+        if (len + len(res) >= m) {
+            var ans = revert(res);
+
+            while (len + 1 < m) {
+                ans = ans.div(10);
+                len++;
+            }
+            return ans.rem(10);
+        }
+        len = len + len(res)
+        i++
+    }
+}
+
+
+fun main(args: Array<String>) {
+    println(digitNumber(1000))
+    println(fib(10))
+    println(lcm(3, 4))
+    println(minDivisor(7))
+    println(maxDivisor(7))
+    println(isCoPrime(25, 49))
+    println(squareBetweenExists(26, 29))
+    println(collatzSteps(15))
+    println(sin(1.77, 0.00001))
+    println(cos(1.77, 0.00001))
+    println(revert(123456))
+    println(isPalindrome(123321))
+    println(hasDifferentDigits(11211))
+    for (i in 1..10)
+        println(fibSequenceDigit(i))
+}
