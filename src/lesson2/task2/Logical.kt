e0df2179b0d77f4e3fa9a1779cf51d8a49821ee0
Vladislav <43061844+triadecom@users.noreply.github.com>
2018-11-07 15:57:43 +0300

    Add files via upload

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -44,12 +44,9 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = abs(x1 - x2) =
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int {
-
-    return if (((month + month / 8) % 2 == 0) && (month != 2)) 30
-    else if (month == 2) if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) && (month == 2)) 29 else 28
-    else 31
-}
+fun daysInMonth(month: Int, year: Int): Int = if (((month + month / 8) % 2 == 0) && (month != 2)) 30
+else if (month == 2) if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) && (month == 2)) 29 else 28
+else 31
 
 
 /**
